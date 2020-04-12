73cbd7f0a6041005103ee8e4306eaa929b13f0c7
Xperthobbit <bryson.gariepy@gmail.com>
2020-04-11 22:29:54 -0400

    Added kang command

---
+++ cmds/kinglevel.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js'); /* Include this if you use embeds, etc. */
+
+module.exports.run = async (Client, message, args) => {
+    /* Insert command code here */
+    let king = function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+    message.channel.send(`Your KANG level is: ${king(100)}. :crown:`);
+};
+
+module.exports.help = {
+  name: 'kinglevel', // <-- Command name (what the user types after prefix)
+  usage: 'Checks your king level' // <-- Usage (not nessasary but you can include if you need to)
+};
