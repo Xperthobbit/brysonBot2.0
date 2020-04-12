5c74483c5ac30dd2b402585792093fa997712ba5
Xperthobbit <bryson.gariepy@gmail.com>
2020-04-12 10:00:15 -0400

    Added role for level 100 'kangz'

--- cmds/kinglevel.js
+++ cmds/kinglevel.js
@@ -3,8 +3,8 @@ const Discord = require('discord.js'); /* Include this if you use embeds, etc. *
 const used = new Set();
 
 module.exports.run = async (Client, message, args) => {
-  /* Insert command code here */
-  
+	/* Insert command code here */
+
 	let king = function getRandomInt(max) {
 		return Math.floor(Math.random() * Math.floor(max));
 	};
@@ -12,11 +12,24 @@ module.exports.run = async (Client, message, args) => {
 	if (used.has(message.author.id)) {
 		return message.reply('Wait 12 hours  before using this command again');
 	} else {
-    message.channel.send(`Your KANG level is: ${king(101)}. :crown:`);
-    used.add(message.author.id)
-    setTimeout(() => {
-      used.delete(used.message.author.id)
-    }, 1000 * 60 * 60 * 12);
+		let level = king(101);
+		message.channel.send(`Your KANG level is: ${level}. :crown:`);
+		used.add(message.author.id);
+		setTimeout(() => {
+			used.delete(used.message.author.id);
+		}, 1000 * 60 * 60 * 12);
+		if (level === 100) {
+			let role = message.guild.roles.find((role) => role.name == 'KANGZ');
+			let Member = message.guild.member(message.author);
+			await Member.addRole(role);
+			try {
+				message.reply(
+					`Congrats Kang! You hit :100: so you get the KANGZ role! :crown:`
+				);
+			} catch (err) {
+				console.log('Role not found on server! | ' + err.stack);
+			}
+		}
 	}
 };
 
