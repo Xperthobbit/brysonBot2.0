757331847bd076cb733001baac208015cd13cdc1
Xperthobbit <bryson.gariepy@gmail.com>
2020-04-16 09:18:53 -0400

    Updated to show time remaining on cooldown

--- cmds/kinglevel.js
+++ cmds/kinglevel.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js'); /* Include this if you use embeds, etc. */
 
-const used = new Set();
+const used = new Map();
+const Duration = require('humanize-duration');
 
 module.exports.run = async (Client, message, args) => {
 	/* Insert command code here */
@@ -8,16 +9,17 @@ module.exports.run = async (Client, message, args) => {
 	let king = function getRandomInt(max) {
 		return Math.floor(Math.random() * Math.floor(max));
 	};
-
-	if (used.has(message.author.id)) {
-		return message.reply('Wait 12 hours  before using this command again');
+	const cooldown = used.get(message.author.id);
+	if (cooldown) {
+		const remaining = Duration(cooldown - Date.now(), { units: ['h', 'm'], round: true });
+		return message
+			.reply(`You have to wait ${remaining} before you can run this command again`)
+			.catch(console.error);
 	} else {
 		let level = king(101);
-		message.channel.send(`Your KANG level is: ${level}. :crown:`);
-		used.add(message.author.id);
-		setTimeout(() => {
-			used.delete(used.message.author.id);
-		}, 1000 * 60 * 60 * 12);
+		message.channel.send(`Your KING level is: ${level}. :crown:`);
+		used.set(message.author.id, Date.now() + 1000 * 60 * 60 * 12);
+		setTimeout(() => used.delete(message.author.id), 1000 * 60 * 60 * 12);
 		if (level === 100) {
 			let role = message.guild.roles.find((role) => role.name == 'KANGZ');
 			let Member = message.guild.member(message.author);
@@ -27,7 +29,10 @@ module.exports.run = async (Client, message, args) => {
 					`Congrats Kang! You hit :100: so you get the KANGZ role! :crown:`
 				);
 			} catch (err) {
-				console.log('Role not found on server! Or failed because they have the role | ' + err.stack);
+				console.log(
+					'Role not found on server! Or failed because they have the role | ' +
+						err.stack
+				);
 			}
 		}
 	}
