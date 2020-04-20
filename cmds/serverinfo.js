17489ac5ccd5f8c093c4ccc5b3eb876ac22f3c4e
Xperthobbit <bryson.gariepy@gmail.com>
2020-04-20 09:37:17 -0400

    minor improvments for updating regx

--- cmds/serverinfo.js
+++ cmds/serverinfo.js
@@ -15,9 +15,10 @@ module.exports.run = async (Client, message, args) => {
 		);
 		/* Edit your games with reg ex here */
 	} else if (args[0] === 'minecraft' || args[0] === 'mc') {
+		/* Edit me */
+		args[0] = 'Minecraft';
 		serverInfo.servers.forEach(x => {
-							/* Edit me */
-			if (x.Server === 'Minecraft'){
+			if (x.Server === args[0]){
 				output += `**${x.Server}**:` + '\n' + x.IP;
 				if (x.Port !== ''){
 					output += ':' + x.Port + '\n';
@@ -26,9 +27,11 @@ module.exports.run = async (Client, message, args) => {
 		});
 		/* Edit your games with reg ex here */
 	} else if (args[0] === 'l4d2' || args[0] === 'left4dead2') {
+		/* Edit me */
+		args[0] = 'L4D2';
 		serverInfo.servers.forEach(x => {
 							/* Edit me */
-			if (x.Server === 'L4D2'){
+			if (x.Server === args[0]){
 				output += `**${x.Server}**:` + '\n' + x.IP;
 				if (x.Port !== ''){
 					output += ':' + x.Port + '\n';
@@ -37,9 +40,11 @@ module.exports.run = async (Client, message, args) => {
 		});
 		/* Edit your games with reg ex here */
 	} else if (args[0] === 'csgo' || args[0] === 'counterstrike' || args[0] === 'cs') {
+		/* Edit me */
+		args[0] = 'CSGO';
 		serverInfo.servers.forEach(x => {
 							/* Edit me */
-			if (x.Server === 'CSGO'){
+			if (x.Server === args[0]){
 				output += `**${x.Server}**:` + '\n' + x.IP;
 				if (x.Port !== ''){
 					output += ':' + x.Port + '\n';
