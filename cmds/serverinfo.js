7b8b922e138cd7533a2ef0a747181b4efe952a9d
Xperthobbit <bryson.gariepy@gmail.com>
2020-04-20 09:26:15 -0400

    serverinfo now use json file (thanks maav)

--- cmds/serverinfo.js
+++ cmds/serverinfo.js
@@ -1,41 +1,85 @@
 /* serverinfo.js */
-
+/* Remember to edit the JSON file before editing the code in here! */
 const Discord = require('discord.js');
+const fs = require('fs');
 
 module.exports.run = async (Client, message, args) => {
 	let msg = await message.channel.send('Checking my database...');
-	let serverInfo = '';
+	const serverInfo = require('../serverinfo.json');
+	let output = '';
 
 	if (!args[0]) {
 		msg.delete();
 		return message.reply(
-			'please include a game server you wish to get the details for!'
+			`please include a game server you wish to get the details for! Or type 'all' for all of them.`
 		);
+		/* Edit your games with reg ex here */
 	} else if (args[0] === 'minecraft' || args[0] === 'mc') {
-		serverInfo = '**IP:** mc.cantfraglike.me \n **Port:**';
-	} else if (
-		args[0] === 'l4d2' ||
-		args[0] === 'left4dead2'
-	) {
-		serverInfo = '**IP:** 70.48.151.82 \n **Port:** 27015';
-	} else if (args[0] === 'csgo' || args[0] === 'counterstrike') {
-		serverInfo = '**IP:** 70.48.151.82 \n **Port:** 27066';
+		serverInfo.servers.forEach(x => {
+							/* Edit me */
+			if (x.Server === 'Minecraft'){
+				output += `**${x.Server}**:` + '\n' + x.IP;
+				if (x.Port !== ''){
+					output += ':' + x.Port + '\n';
+				} 
+			}
+		});
+		/* Edit your games with reg ex here */
+	} else if (args[0] === 'l4d2' || args[0] === 'left4dead2') {
+		serverInfo.servers.forEach(x => {
+							/* Edit me */
+			if (x.Server === 'L4D2'){
+				output += `**${x.Server}**:` + '\n' + x.IP;
+				if (x.Port !== ''){
+					output += ':' + x.Port + '\n';
+				} 
+			}
+		});
+		/* Edit your games with reg ex here */
+	} else if (args[0] === 'csgo' || args[0] === 'counterstrike' || args[0] === 'cs') {
+		serverInfo.servers.forEach(x => {
+							/* Edit me */
+			if (x.Server === 'CSGO'){
+				output += `**${x.Server}**:` + '\n' + x.IP;
+				if (x.Port !== ''){
+					output += ':' + x.Port + '\n';
+				} 
+			}
+		});
+	} else if ('all') {
+		serverInfo.servers.forEach((x) => {
+			output += `**${x.Server}**:` + '\n' + x.IP;
+			if (x.Port !== ''){
+				output += ':' + x.Port + '\n';
+			} 
+		});
 	} else {
 		msg.delete();
 		return message.reply(`game server doesn't exist or invalid game!`);
+	}
+
+	function createdDate(file) {
+		const { birthtime } = fs.statSync(file);
+		return birthtime;
 	}
 
 	var gameTitle = args[0].toUpperCase();
 	const embed = new Discord.RichEmbed()
 		.setColor(0x5d2079)
 		.setTitle(`${gameTitle} SERVER INFO:`)
-		.setDescription(serverInfo)
-		.setFooter(`*Information last updated 4/18/2020`);
+		.setDescription(output)
+		.setFooter(
+			`*Information last updated ${createdDate('serverinfo.json')
+				.toISOString()
+				.replace('-', '/')
+				.split('T')[0]
+				.replace('-', '/')}`
+		);
 	message.channel.send(embed).catch((error) => message.reply(`${error}`));
 	msg.delete();
 };
 
 module.exports.help = {
 	name: 'serverinfo',
-	usage: 'serverinfo <game>',
+	usage: 'serverinfo <game> [all]',
 };
