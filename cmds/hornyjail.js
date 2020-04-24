4af99cef4af0f6d921f93950264d43a804ae5493
Xperthobbit <bryson.gariepy@gmail.com>
2020-04-24 09:15:32 -0400

    new command 'hornyjail'

---
+++ cmds/hornyjail.js
@@ -0,0 +1,17 @@
+/* hornyjail.js */
+const Discord = require('discord.js');
+
+module.exports.run = async (Client, message, args) => {
+  let usr = message.mentions.users.first();
+  if (!usr) {
+    message.reply('please tag a user!').catch((err) => message.reply(`${err}`));
+  }
+  else {
+    message.channel.send(`${usr} `, {files: ['https://i.imgur.com/ydUaXNc.jpeg']}).catch((err) => message.reply(`${err}`))
+  }
+};
+
+module.exports.help = {
+  name: 'hornyjail', 
+  usage: 'hornyjail <user>'
+};
