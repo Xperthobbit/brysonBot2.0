0b20154037b091b1a4842b73c518239bc85cee95
Xperthobbit <bryson.gariepy@gmail.com>
2020-05-05 21:27:48 -0400

    updated servers

--- cmds/serverinfo.js
+++ cmds/serverinfo.js
@@ -27,7 +27,7 @@ module.exports.run = async (Client, message, args) => {
 			}
 		});
 		/* Edit me. If you don't have another mc server delete this */
-		args[0] = 'RLCraft';
+		args[0] = 'Hardcore SMP';
 		serverInfo.servers.forEach((x) => {
 			if (x.Server === args[0]) {
 				output += `**${x.Server}**:` + '\n' + x.IP;
