db752d1ea2eca8f678eb115d7b8b618059a0416b
Xperthobbit <bryson.gariepy@gmail.com>
2020-05-29 16:05:05 -0400

    bumped the frequency of who asked to 170

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ Client.on('message', async (message) => {
 	let args = messageCont.slice(1);
 	if (!command.startsWith(prefix)) {
 		let who = function getRandomInt() {
-			return Math.floor(Math.random() * Math.floor(70));
+			return Math.floor(Math.random() * Math.floor(170));
 		};
 
 		let check = who();
