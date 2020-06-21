03b4b08aba59d87c14f273b3239417f6ce2fef29
bryson.dev <bryson.gariepy@gmail.com>
2020-06-20 23:22:08 -0400

    added grant count; version 1.4.2

---
+++ cmds/grantcount.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+module.exports.run = async (Client, message, args) => {
+    /* Insert command code here */
+    let count = JSON.parse(fs.readFileSync('./count.json', 'utf8'));
+
+    let curcount = count.count;
+
+    message.channel.send(`Grant has said he'll stop but hasn't ${curcount} times.`);
+
+    curcount = {
+      count: curcount + 1
+    };
+
+    fs.writeFile('./count.json', JSON.stringify(curcount), (err) => {
+        if (err) console.log(err);
+    });
+};
+
+module.exports.help = {
+  name: 'grantcount', 
+  usage: 'grantcount' 
+};
